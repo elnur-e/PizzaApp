8f78397bd85a60571ec1c19c74807b4a4d91fc2a
Melike244 <makhmut.malike@std.izu.edu.tr>
2025-04-29 13:19:42 +0300

    fix

--- lib/components/food_tile.dart
+++ lib/components/food_tile.dart
@@ -5,6 +5,7 @@ import '../models/food.dart';
 class FoodTile extends StatelessWidget {
   final Food food;
   final void Function()? onTap;
+
   const FoodTile({
     super.key,
     required this.food,
@@ -13,7 +14,6 @@ class FoodTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-<<<<<<< HEAD
     return GestureDetector(
       onTap: onTap,
       child: Container(
@@ -26,93 +26,34 @@ class FoodTile extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            // image
+            // Görsel
             Image.asset(
               food.imagePath,
               height: 140,
             ),
 
-            //  const SizedBox(width: 20),
-            // text
+            // Yemek ismi
             Text(
               food.name,
               style: GoogleFonts.dmSerifDisplay(fontSize: 20),
             ),
 
-            // price + rating
+            // Fiyat ve puan
             SizedBox(
               width: 160,
               child: Row(
-
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  //price
+                  // Fiyat
                   Text(
-                    '\$' + food.price,
+                    '₺' + food.price,
                     style: TextStyle(
                       fontWeight: FontWeight.bold,
                       color: Colors.grey[800],
                     ),
                   ),
-                  //rating
 
-
-                  /*   Icon(
-=======
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.grey[100],
-        borderRadius: BorderRadius.circular(20),
-      ),
-      margin: const EdgeInsets.only(left: 25.0),
-      padding: const EdgeInsets.all(25),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          // image
-          Image.asset(
-            food.imagePath,
-            height: 140,
-          ),
-
-        //  const SizedBox(width: 20),
-          // text
-          Text(
-            food.name,
-            style: GoogleFonts.dmSerifDisplay(fontSize: 20),
-          ),
-
-          // price + rating
-          SizedBox(
-            width: 160,
-            child: Row(
-
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                //price
-                Text(
-                  '₺' + food.price,
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.grey[800],
-                  ),
-                ),
-                  //rating
-
-
-             /*   Icon(
->>>>>>> 6c9a56dc249fa1fa75cb0924bf26555edaf77d6e
-                  Icons.star,
-                  color.yellow[800]
-                ),
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Colors.grey[700],
-                  ),
-                ),
-*/
-<<<<<<< HEAD
-                  //rating
+                  // Puan (rating)
                   Row(
                     children: [
                       Icon(
@@ -130,25 +71,6 @@ class FoodTile extends StatelessWidget {
             )
           ],
         ),
-=======
-                //rating
-                Row(
-                  children: [
-                    Icon(
-                      Icons.star,
-                      color: Colors.yellow[800],
-                    ),
-                    Text(
-                      food.rating,
-                      style: TextStyle(color: Colors.grey),
-                    ),
-                  ],
-                )
-              ],
-            ),
-          )
-        ],
->>>>>>> 6c9a56dc249fa1fa75cb0924bf26555edaf77d6e
       ),
     );
   }
